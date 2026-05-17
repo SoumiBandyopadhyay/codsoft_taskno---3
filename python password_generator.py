c790db5868d3b527324a3f0765427a90930a1d65
Soumi060 <soumibandyopadhyay340@gmail.com>
2026-05-17 20:03:59 +0530

    Add files via upload

---
+++ python password_generator.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+# Function to generate password
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+# User input
+length = int(input("Enter the desired password length: "))
+
+# Generate and display password
+password = generate_password(length)
+
+print("\nGenerated Password:", password)
